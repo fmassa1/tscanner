e576010aa9b222989c6b10ee4e16e873d65e41c3
Fredric Massa <rickmass2020@gmail.com>
2025-09-26 12:40:08 -0500

    basic reddit scrapper

--- backend/scripts/reddit_scanner.py
+++ backend/scripts/reddit_scanner.py
@@ -1,31 +1,58 @@
 import requests
 
-reddit_urls = [   "https://www.reddit.com/r/uichicago/.json",
-               ]
+reddit_urls = ["https://www.reddit.com/r/uichicago/.json"]
 headers = {"User-Agent": "MyRedditApp/0.1"}
 
-reddit_data = []
 posts_data = []
 
-for url in reddit_urls:
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        children = data["data"]["children"]
+def get_posts():
+    for url in reddit_urls:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            children = data["data"]["children"]
 
-        for child in children:
-            post = child["data"]
-            posts_data.append({
-                "title": post.get("title"),
-                #"selftext": post.get("selftext"),
-                "author": post.get("author"),
-                "author_fullname": post.get("author_fullname"),
-                "num_comments": post.get("num_comments"),
-                "ups": post.get("ups"),
-            })
-    else:
-        print(f"Failed to fetch {url}: {response.status_code}")
+            for child in children:
+                post = child["data"]
+                posts_data.append({
+                    "title": post.get("title"),
+                    "selftext": post.get("selftext"),
+                    "author": post.get("author"),
+                    "author_fullname": post.get("author_fullname"),
+                    "num_comments": post.get("num_comments"),
+                    "ups": post.get("ups"),
+                    "url": post.get("url"),
+                    "permalink": f"https://www.reddit.com{post.get('permalink')}",
+                    "comments": []
+                })
+        else:
+            print(f"Failed to fetch {url}: {response.status_code}")
 
-print(len(posts_data))
+
+def check_posts():
+    for post in posts_data:
+        response = requests.get(post.url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            comments_data = data[1]["data"]["children"]
+            comments = []
+
+            for c in comments_data:
+                if c["kind"] == "t1":
+                    comment = c["data"]
+                    comments.append({
+                        "name": comment.get("author"),
+                        "author": comment.get("author"),
+                        "body": comment.get("body"),
+                        "ups": comment.get("ups"),
+                        "id": comment.get("id"),
+                        "parent_id": comment.get("parent_id"),
+                    })
+            
+            post["comments"] = comments
+
+get_posts()
+check_posts()
+print(len(posts_data[:5]))
 for p in posts_data:
     print(p)

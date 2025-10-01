baa903f55d8ca75e6780d8ed8299157f564891cf
Fredric Massa <rickmass2020@gmail.com>
2025-10-01 12:52:42 -0500

    added a page to look at all user comments and posts

--- backend/server.py
+++ backend/server.py
@@ -10,6 +10,13 @@ def index():
     users = getAllUsers()
     return render_template("home.html", users=users)
 
+
+@app.route("/user/<username>")
+def user_page(username):
+    posts = getUsersPosts(username)
+    comments = getUsersComments(username)
+    return render_template("user.html", username=username, posts=posts, comments=comments)
+
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=5000, debug=True)
     #app.run(debug=True)
